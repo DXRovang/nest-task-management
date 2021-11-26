3226a8a6e1915ed2cfd947bd9053e13bf073e3ec
Alexander Rovang <DXRovang@gmail.com>
2021-11-26 15:55:19 -0500

    adds task.model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [1,2,3];
+  
+  getAllTasks(){
+    return this.tasks
+  }
+  getOneTask(){
+    return this.tasks[0]
+  }
+}
